0095dd0109a23911de6f51f3e4b4fb65fc57695c
Jacob LeCoq <lecoqjacob@gmail.com>
2021-03-21 20:45:31 -0500

    init

---
+++ JustWalking/JustWalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkingApp.swift
+//  JustWalking
+//
+//  Created by Jacob LeCoq on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
